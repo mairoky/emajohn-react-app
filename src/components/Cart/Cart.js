dccffae3944ee4e1b161e107aa20fd7097b0ccb5
Aminul Islam Rocky <mairoky@outlook.com>
2022-09-27 20:23:15 +0600

    cart functionality added

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDeleteLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import './Cart.css'
+
+const Cart = ({ cart }) => {
+    let total = 0;
+    let shipping = 0;
+    let quantity = 0;
+    for (const product of cart) {
+        quantity = quantity + product.quantity;
+        total = total + (product.price * product.quantity);
+        shipping = shipping + product.shipping;
+    }
+    const tax = (total * 0.1).toFixed(2);
+    const grandTotal = total + shipping + parseFloat(tax);
+    return (
+        <div className="cart">
+            <h2>Order Summary</h2>
+            <div className="cart-details">
+                <p>Selected Items: {quantity}</p>
+                <p>Total Price: ${total}</p>
+                <p>Total Shipping Charge: ${shipping}</p>
+                <p>Tax: ${tax}</p>
+                <h3>Grand Total: ${grandTotal}</h3>
+            </div>
+            <button>
+                <p>Clear Cart</p>
+                <FontAwesomeIcon icon={faDeleteLeft}></FontAwesomeIcon>
+            </button>
+            <button>
+                <p>Review Order</p>
+                <FontAwesomeIcon icon={faArrowRight}></FontAwesomeIcon>
+            </button>
+        </div>
+    );
+};
+
+export default Cart;
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDeleteLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { addToDb, getFromDb } from '../../utilities/fakedb';
+import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
 
@@ -12,11 +12,35 @@ const Shop = () => {
             .then(res => res.json())
             .then(data => setProducts(data))
     }, []);
+    // get cart data from local storage
+    useEffect(() => {
+        const storedCart = getFromDb();
+        const saveCart = [];
+        for (const id in storedCart) {
+            const addedProduct = products.find(product => product.id === id);
+            if (addedProduct) {
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                saveCart.push(addedProduct);
+            }
+        }
+        setCart(saveCart);
+    }, [products]);
     // Event handle for product cart button
-    const handleAddToCart = (product) => {
+    const handleAddToCart = (selectedProduct) => {
         // console.log(product);
-        const newCart = [...cart, product];
+        let newCart = [];
+        const exists = cart.find(product => product.id === selectedProduct.id);
+        if (!exists) {
+            selectedProduct.quantity = 1;
+            newCart = [...cart, selectedProduct];
+        } else {
+            const rest = cart.filter(product => product.id !== selectedProduct.id);
+            exists.quantity = exists.quantity + 1;
+            newCart = [...rest, exists];
+        }
         setCart(newCart);
+        addToDb(selectedProduct.id);
     }
     return (
         <div className="shop">
@@ -30,22 +54,7 @@ const Shop = () => {
                 }
             </div>
             <div className="cart-container">
-                <h2>Order Summary</h2>
-                <div className="cart-details">
-                    <p>Selected Items: {cart.length}</p>
-                    <p>Total Price: $1140</p>
-                    <p>Total Shipping Charge: $5</p>
-                    <p>Tax: $114</p>
-                    <h3>Grand Total: $1559</h3>
-                </div>
-                <button>
-                    <p>Clear Cart</p>
-                    <FontAwesomeIcon icon={faDeleteLeft}></FontAwesomeIcon>
-                </button>
-                <button>
-                    <p>Review Order</p>
-                    <FontAwesomeIcon icon={faArrowRight}></FontAwesomeIcon>
-                </button>
+                <Cart cart={cart}></Cart>
             </div>
         </div>
     );
